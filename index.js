fca1ce399c7ace1e41ee0a7a3795d8556d21bf83
Jake <jakerfilut@gmail.com>
2021-11-01 16:17:38 -0600

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
+  if (feet <= 400){
+    return 'This one is on me!'
+  } if (feet > 2000 && feet <= 2500){
+    return 'I will gladly take your thirty bucks.'
+  } if (feet > 2500){
+    return 'No can do.'
+  }
   // Write your code here!
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
+  return(city === 'NYC' ? 'Ok, sounds good.' : 'No go.')
   // Write your code here!
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case 'generous':
+      return "Thank you so much."
+      break;
+    case 'not as generous':
+      return "Thank you."
+      break;
+    default:
+      return "Bye."
+      break;
+  }
   // Write your code here!
 }
